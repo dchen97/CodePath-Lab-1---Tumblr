8f35a660b48da7c65aa40715543fda76ab81c516
Diana <crystaljerry@gmail.com>
2017-01-31 19:24:38 -0700

    Add work from the tutorial

---
+++ Pods/Target Support Files/Pods-codepath-lab1-tumblr/Pods-codepath-lab1-tumblr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_codepath_lab1_tumblr : NSObject
+@end
+@implementation PodsDummy_Pods_codepath_lab1_tumblr
+@end
---
+++ codepath-lab1-tumblr/PhotoCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCell.swift
+//  codepath-lab1-tumblr
+//
+//  Created by Diana C on 1/31/17.
+//  Copyright © 2017 Diana C. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
